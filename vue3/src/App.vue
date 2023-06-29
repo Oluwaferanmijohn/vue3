091f11c6d30f71fdc37e819dfd8a843b55087d0a
Oluwaferanmi oluwatoyin <106152705+Oluwaferanmijohn@users.noreply.github.com>
2023-06-29 14:10:35 +0100

    Add files via upload

---
+++ vue3/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { onMounted } from "vue";
+import { initFlowbite } from "flowbite";
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+
+<template>
+
+</template>
+
+<style></style>
---
+++ vue3/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  plugins: [require("flowbite/plugin")],
+
+  content: [
+    "./node_modules/flowbite/**/*.js, ./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
